933854be47eeddd9dd04caee591d399c76933c05
n-fujihara7174 <n.fujihara7174@gmail.com>
2021-12-06 07:21:50 +0900

    add search function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,15 @@
 class UsersController < ApplicationController
 
     def index
-        if params[:searchParam].present? then
-            logger.debug "パラメタ確認用 : #{params[:searchParam]}"
+        if params[:is_search].present? then
+            logger.debug "パラメタ確認用 : #{params.require(:user_name)}"
+            if (params.require(:user_name).present?) then
+                query_text = "(user_name like ?)"
+            else
+                query_text = "(user_name like ? or True)"
+            end
             @users = User.joins(:post).select('users.*,posts.*')\
-            .where("user_name = ? and display_user_name = ? ", search_param.user_name, search_param.display_user_name)
+            .where("user_name like ? and display_user_name like ? ", "%" + search_param.user_name + "%", "%" + search_param.display_user_name + "%")
         else
             @users = User.joins(:post).select('users.*,posts.*')
         end
@@ -17,9 +22,5 @@ class UsersController < ApplicationController
     end
 
     private
-
-    def search_param
-        params.require(:searchParam).permit(:userName,:userId)
-    end
 
 end
