85d2607ad9e90b4e78cebd28e710c1ef331205af
n-fujihara7174 <n.fujihara7174@gmail.com>
2021-12-29 23:35:36 +0900

    編集画面のユーザー情報返却機能作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,6 @@
 class UsersController < ApplicationController
 
     def index
-        #logger.debug "パラメタ確認用 : #{params.require(:user_name)}"
-
         @users = User.join_post_all
         #logger.debug "user.all : #{@users.size}"
         @users = @users.search_user_id_or_email(params[:user_id_or_email]) if params[:user_id_or_email].present?
@@ -10,7 +8,7 @@ class UsersController < ApplicationController
     end
 
     def show
-        @user = User.joins(:post).select('users.*,posts.*').find(params[:id])
+        @user = User.join_post_find_id(params[:id])
         render :json => @user
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,15 @@ class User < ApplicationRecord
     #end
 
     def self.join_post_all()
-        User.joins(:post).select('users.*, count(posts.id) as post_count').group('users.id').order(user_id: :ASC)
+        User.select('users.*, count(posts.id) as post_count').joins(:post).group('users.id').order(user_id: :ASC)
     end
 
     def self.search_user_id_or_email(parameter)
         #Rails.logger.debug "book.delete_flg #{self.joins(:post).select('users.*,posts.*').where("user_name like ?", "%#{user_name}%").size}"
-        users = self.joins(:post).select('users.*,posts.*').where("user_id like ? or email like ?", "%#{parameter.to_s}%", "%#{parameter.to_s}%")
+        users = self.select('users.*, count(posts.id) as post_count').joins(:post).where("users.user_id like ? or email like ?", "%#{parameter.to_s}%", "%#{parameter.to_s}%").group('users.id').order(user_id: :ASC)
+    end
+
+    def self.join_post_find_id(id)
+        User.select('users.*, count(posts.id) as post_count').joins(:post).where('users.id = ?', id).group('users.id').order(user_id: :ASC)
     end
 end
